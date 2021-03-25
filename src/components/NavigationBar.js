2ba093e60ff6ab0cb80898362f0fffc52d749f23
adornala16382 <R.E.N.dornala@gmail.com>
2021-03-25 12:31:37 -0400

    Home page fix
    Added designs to the homepage and made it responsive to different screen sizes.

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,21 +4,20 @@ import {Link, HashRouter as Router} from "react-router-dom";
 import "../styles.css";
 export const NavigationBar = () => (
 
-  <Navbar expand = "lg" >
-  <Navbar.Brand></Navbar.Brand>
+  <Navbar expand = "md">
   <Navbar.Toggle aria-controls="basic-navbar-nav">
 
   </Navbar.Toggle>
   <Navbar.Collapse id="basic-navbar-nav" >
 
-    <Nav class="navbar-nav nav-fill w-100" >
+    <Nav class="navbar-nav nav-fill w-100 " >
      <Router>
       
       <Nav.Item><Nav.Link as = {Link} to = "/"><h5>Home</h5></Nav.Link></Nav.Item>
       <Nav.Item><Nav.Link as = {Link} to = "/mentorship"><h5>Mentorship</h5></Nav.Link></Nav.Item>
       <Nav.Item><Nav.Link as = {Link} to = "/employment"><h5>Employment</h5></Nav.Link></Nav.Item>
       <Nav.Item><Nav.Link as = {Link} to = "/resources"><h5>Resources</h5></Nav.Link></Nav.Item>
-      <Nav.Item><Nav.Link as = {Link} to = "/app"><h5>App</h5></Nav.Link></Nav.Item>
+      <Nav.Item><Nav.Link as = {Link} to = "/mobileapp"><h5>App</h5></Nav.Link></Nav.Item>
       <Nav.Item><Nav.Link as = {Link} to = "/signin"><h5>Sign In</h5></Nav.Link></Nav.Item>
       
       </Router>
